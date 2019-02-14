dabb073221f2f3cd75c2b6a05e4ac52a4d0f56e0
Andrew Murray <darbyguitars@gmail.com>
2019-02-14 11:22:33 -0600

    inital commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Home from './home';
+import Procedures from './procedures';
+import Contact from './contact';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <Link to="/">Home</Link>{' '}
+            <Link to="/procedures">Procedures</Link>{' '}
+            <Link to="/contact">Contact</Link>
+          </nav>
+          <Route exact path="/" component={Home} />
+          <Route path="/procedures" component={Procedures} />
+          <Route path="/contact" component={Contact} />
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default App
